114089f4b430c206617fbec5ad1fb34b4f806cfc
Chris Hewitt <agop5134@gmail.com>
2022-03-07 11:56:34 -0700

    add small service test, final commit

---
+++ spec/services/book_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe "book service", :vcr do
+  it "gets info for a book title containing search params" do
+    search_param = "denver, co"
+    limit = "5"
+
+    response = BookService.search(search_param, limit)
+    expect(response[:docs].length).to eq(5)
+  end
+end
