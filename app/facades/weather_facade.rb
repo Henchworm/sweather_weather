6d8490cd6a0f8c5cbc82095970bd7a3310f33c26
Chris Hewitt <agop5134@gmail.com>
2022-03-06 17:38:21 -0700

    complete forecast poro w/ 3 weather attributes: current, hourly, daily

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -5,7 +5,7 @@ class WeatherFacade
     time_range_values = {
       current_weather: response[:current],
       hourly_weather: response[:hourly].first(8),
-      daily_weather: response[:daily]
+      daily_weather: response[:daily].first(5)
     }
     Forecast.new(time_range_values)
   end
--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -4,6 +4,57 @@ class Forecast
               :hourly_weather,
               :daily_weather
 
-  def initiailize(values)
-  end 
+  def initialize(time_range_values)
+    @id = nil
+    @current_weather = current_formatter(time_range_values[:current_weather])
+    @hourly_weather = hourly_formatter(time_range_values[:hourly_weather])
+    @daily_weather = daily_formatter(time_range_values[:daily_weather])
+    binding.pry
+  end
+
+  def current_formatter(data)
+    {
+      datetime: Time.at(data[:dt]),
+      sunrise: Time.at(data[:sunrise]),
+      sunset: Time.at(data[:sunset]),
+      temperature: data[:temp],
+      feels_like: data[:feels_like],
+      humiditiy: data[:humidity],
+      uvi: data[:uvi],
+      visibility: data[:visibility],
+      conditions: data[:weather].first[:description],
+      conditions: data[:weather].first[:icon]
+    }
+  end
+
+  def hourly_formatter(data)
+    data.map do |hour|
+      {
+        time: Time.at(hour[:dt]).strftime('%T'),
+        temperature: hour[:temp].to_f,
+        conditions: hour[:weather][0][:description],
+        icon: hour[:weather][0][:icon]
+      }
+    end
+  end
+
+  def daily_formatter(data)
+    data.map do |day|
+    {
+      date: Time.at(day[:dt]).strftime('%F'),
+      sunrise: Time.at(day[:sunrise]),
+      sunset: Time.at(day[:sunset]),
+      max_temp: day[:temp][:max],
+      min_temp: day[:temp][:min],
+      conditions: day[:weather][0][:description],
+      icon: day[:weather][0][:icon]
+    }
+    end
+  end
+
+
+# uvi, numeric (int or float), as given by OpenWeather
+# visibility, numeric (int or float), as given by OpenWeather
+# conditions, the first ‘description’ field from the weather data as given by OpenWeather
+# icon, string, as given by OpenWeather
 end
