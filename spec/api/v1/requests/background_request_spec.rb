552b1aa92ef40c350fbd13ac6684ed81017d2bd8
Chris Hewitt <agop5134@gmail.com>
2022-03-07 21:25:03 -0700

    update specs, new VCR

--- spec/api/v1/requests/background_request_spec.rb
+++ spec/api/v1/requests/background_request_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "request for background image" do
     expect(parsed[:data][:type]).to eq("background")
     expect(parsed[:data][:attributes][:description]).to eq("Blue Bear Looking In")
     expect(parsed[:data][:attributes][:location]).to eq("denver")
-    expect(parsed[:data][:attributes][:url]).to eq("https://images.unsplash.com/photo-1511286148006-ec48824e3282?ixid=MnwzMDc4ODF8MHwxfHNlYXJjaHwxfHxkZW52ZXIlMkNjb3NreWxpbmV8ZW58MHx8fHwxNjQ2NjE4OTM2&ixlib=rb-1.2.1")
+    expect(parsed[:data][:attributes][:url]).to eq("https://images.unsplash.com/photo-1511286148006-ec48824e3282?ixid=MnwzMDc4ODF8MHwxfHNlYXJjaHwxfHxkZW52ZXIlMkNjb3NreWxpbmV8ZW58MHx8fHwxNjQ2NzEzMzcx&ixlib=rb-1.2.1")
     expect(parsed[:data][:attributes][:credits][:username]).to eq("mirandafayj")
     expect(parsed[:data][:attributes][:credits][:portfolio_link]).to eq("http://www.mirandafayj.com")
     expect(parsed[:data][:attributes][:credits][:upsplash_link]).to eq("https://api.unsplash.com/photos/lkLTeBY2SZE")
