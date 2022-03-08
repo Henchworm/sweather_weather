57ba512b6f2115cebb1206404fbea9d65a5df01d
Chris Hewitt <agop5134@gmail.com>
2022-03-08 12:06:05 -0700

    handroll forecast serializer to include limit 8

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::ForecastController < ApplicationController
   def index
     coords = MapQuestFacade.geocode(params["location"])
     forecast = WeatherFacade.forecast(coords)
-    render(json: ForecastSerializer.new(forecast))
+    render(json: ForecastSerializer.weather(forecast))
   end
 end
--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,9 +1,47 @@
 class ForecastSerializer
-  include JSONAPI::Serializer
 
-  set_id :id
-  set_type "forecast"
-
-  attributes :current_weather, :daily_weather, :hourly_weather
-
+  def self.weather(forecast)
+    { "data":
+      {
+        "id": nil,
+        "type": 'forecast',
+        "attributes":
+         {
+            "current_weather":
+            {
+              "datetime": forecast.current_weather[:datetime],
+              "sunrise": forecast.current_weather[:sunrise],
+              "sunset": forecast.current_weather[:sunset],
+              "temperature": forecast.current_weather[:temperature],
+              "feels_like": forecast.current_weather[:feels_like],
+              "humidity": forecast.current_weather[:humidity],
+              "uvi": forecast.current_weather[:uvi],
+              "visibility": forecast.current_weather[:visibility],
+              "conditions": forecast.current_weather[:condition],
+              "icon": forecast.current_weather[:icon]
+            },
+            "daily_weather": forecast.daily_weather.map do |day|
+                {
+                  "date": day[:date],
+                  "sunrise": day[:sunrise],
+                  "sunset": day[:sunset],
+                  "max_temp": day[:max_temp],
+                  "min_temp": day[:min_temp],
+                  "conditions": day[:conditions],
+                  "icon": day[:icon]
+                }
+            end,
+            "hourly_weather": forecast.hourly_weather.first(8).map do |hour|
+              {
+              "time": hour[:time],
+              "temperature": hour[:temperature],
+              "conditions": hour[:conditions],
+              "icon": hour[:icon]
+              }
+            end
+          },
+      },
+    }
+  end
 end
+
