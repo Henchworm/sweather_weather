64b60f77f76d9c37d703eef8edc653dd4e5983a2
Chris Hewitt <agop5134@gmail.com>
2022-03-08 12:29:55 -0700

    roadtrip poro spec

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 RSpec.describe RoadTrip do
 
-  it "creates a roadtrip from multiple data types" do
-    
+  it "creates a roadtrip from multiple data types happy path", :vcr do
+    origin = "denver,co"
+    destination = "pueblo,co"
+    roadtrip = RoadTripFacade.new_road_trip(origin, destination)
+    expect(roadtrip).to be_a(RoadTrip)
+    expect(roadtrip.arrival_weather).to be_a(Hash)
+    expect(roadtrip.end_city).to eq("pueblo,co")
+    expect(roadtrip.start_city).to eq("denver,co")
+    expect(roadtrip.travel_time).to eq("01:45:23")
+  end
+
+  it "creates a roadtrip from multiple data types sad path", :vcr do
+    origin = "denver,co"
+    destination = "honolulu,hi"
+    roadtrip = RoadTripFacade.new_road_trip(origin, destination)
+    expect(roadtrip).to be_a(RoadTrip)
+    expect(roadtrip.arrival_weather).to eq("No Data.")
+    expect(roadtrip.travel_tine).to eq("Impossible route.")
   end
 end
