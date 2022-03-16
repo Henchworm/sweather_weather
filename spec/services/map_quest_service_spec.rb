ff55c93ebbe92f80410f563c23b61c2fcd8fe181
Chris Hewitt <agop5134@gmail.com>
2022-03-16 12:01:26 -0600

    comment out mapquest service test

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -1,12 +1,12 @@
-require 'rails_helper'
-RSpec.describe MapQuestService do
-  xit "returnsgeocoded data for city", :vcr do
-    data = "denver,co"
-    response = MapQuestService.geocode(data)
-
-    expect(response).to be_a(Hash)
-    response[:results].each do |result|
-      expect(result[:locations].first[:displayLatLng]).to eq({:lat=>39.738453, :lng=>-104.984853})
-    end
-  end
-end
+# require 'rails_helper'
+# RSpec.describe MapQuestService do
+#   xit "returnsgeocoded data for city", :vcr do
+#     data = "denver,co"
+#     response = MapQuestService.geocode(data)
+#
+#     expect(response).to be_a(Hash)
+#     response[:results].each do |result|
+#       expect(result[:locations].first[:displayLatLng]).to eq({:lat=>39.738453, :lng=>-104.984853})
+#     end
+#   end
+# end
